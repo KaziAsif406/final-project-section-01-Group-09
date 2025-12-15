7be6135b7a847c0b0849e1f268247e30fd2a3b22
Saif20888 <mohammadshahriar95@gmail.com>
2025-12-15 23:03:11 +0600

    minor debugging

--- lib/screens/player_names_screens.dart
+++ lib/screens/player_names_screens.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../models/game_state.dart';
+import '../state_control/game_state.dart';
 import 'game_board_screen.dart';
 
 class PlayerNamesScreen extends StatefulWidget {
